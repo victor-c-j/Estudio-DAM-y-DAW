17bfef22d0764a96153f18f3e52eabb99394ed41
victor-c-j <victor.colorado.jimenez.al@iespoligonosur.org>
2025-09-25 16:31:48 +0200

    Inicio de nuevo ejercicio edad

--- dam/programacion/U1/ejer_u1/src/main/java/com/example/edad.java
+++ dam/programacion/U1/ejer_u1/src/main/java/com/example/edad.java
@@ -6,8 +6,21 @@ public class edad {
     public static void main(String[] args) {
         // https://stackoverflow.com/questions/10873590/using-switch-statement-with-a-range-of-value-in-each-case
         // link for ranged values alternativess
+
+        /*
+         * Ejercicio:
+         * Hacer un programa que indique la denominación en cuanto a su edad de una
+         * persona según los siguientes criterios:
+         * 
+         * Cuando la edad es menor de 14 años, es un niño.
+         * Cuando la edad es de 14 a 22, es un adolescente.
+         * Cuando la edad es de 23 a 43, es un joven.
+         * Cuando la edad es de 44 a 64, es un adulto joven.
+         * Cuando la edad es de 65 a más es un adulto mayor.
+         */
+        
         System.out.println("Mind telling me your age, pretty please? ");
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
 
     }
